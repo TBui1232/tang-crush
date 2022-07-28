9883c39c2df37f981af566829ba3deddcfadaddf
TBui1232 <110125937+TBui1232@users.noreply.github.com>
2022-07-28 19:34:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,17 +2,17 @@ const CONFIG = {
     introTitle: 'Nml!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao Nml cứ đi nhầm đường
-    Lạc vào tim em
+    vào tim em
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích Nml <33',
     btnNo: 'Không, Nml à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu Nml <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại iu Nml <3',
     btnReply: 'Gửi cho Nml <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Nml biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    mess: 'Nml biết mà 🥰. iu Nml nhiều nhiều 😘😘',
     messDesc: 'Nml làm ng iu em nhé.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
